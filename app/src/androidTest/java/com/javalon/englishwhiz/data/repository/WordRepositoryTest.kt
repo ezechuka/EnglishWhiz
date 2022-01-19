f14a434d9a478dc014d7f81fe3b78c9c9c9bb8a4
ezechuka <ezechukwuka9@gmail.com>
2022-01-19 19:45:49 +0100

    added test case for WordModelDao.

---
+++ app/src/androidTest/java/com/javalon/englishwhiz/data/repository/WordRepositoryTest.kt
@@ -0,0 +1,55 @@
+package com.javalon.englishwhiz.data.repository
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.common.truth.Truth.assertThat
+import com.javalon.englishwhiz.data.WordModelDatabase
+import com.javalon.englishwhiz.data.local.WordModelDao
+import com.javalon.englishwhiz.data.local.entity.Label
+import com.javalon.englishwhiz.data.local.entity.Meaning
+import com.javalon.englishwhiz.domain.model.WordModel
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class WordRepositoryTest {
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+    private lateinit var wordModelDatabase: WordModelDatabase
+    private lateinit var wordModelDao: WordModelDao
+
+    @Before
+    fun setUp() {
+        wordModelDatabase = Room.inMemoryDatabaseBuilder(
+            ApplicationProvider.getApplicationContext(),
+            WordModelDatabase::class.java
+        ).allowMainThreadQueries()
+            .build()
+        wordModelDao = wordModelDatabase.wordModelDao()
+    }
+
+    @After
+    fun tearDown() {
+        wordModelDatabase.close()
+    }
+
+    @Test
+    fun insertWordModel() = runBlocking {
+        val meaning =
+            Meaning("definition", "example", "id",
+                "speechPart", synonyms = listOf("synon", "yms"),
+                labels = listOf(Label(true, "label", null)
+                ))
+        val wordModelEntity = WordModel(listOf(meaning), "word", "23dfd31").toWordModelEntity()
+        wordModelDao.insertWordModel(wordModelEntity)
+        val result = wordModelDao.getAllBookmark()
+        assertThat(result).contains(wordModelEntity)
+    }
+}
